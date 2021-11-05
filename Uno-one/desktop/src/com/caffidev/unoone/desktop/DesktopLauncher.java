a402da0c89bc9a071acb18dfc445009b9371c584
caffidev <caffidev@gmail.com>
2021-11-05 13:04:33 +1000

    Initial project: after creating

---
+++ Uno-one/desktop/src/com/caffidev/unoone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.caffidev.unoone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.caffidev.unoone.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
