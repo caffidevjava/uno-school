fa1b77f41965571ac1c7e20c7f24c53def553a2d
caffidev <caffidev@gmail.com>
2021-11-09 18:32:51 +1000

    Renamed folder, created few classes. Firstly I will make a game, then I will rewrite it with use of git commits

---
+++ Uno/core/src/com/caffidev/unoone/Board.java
@@ -0,0 +1,5 @@
+package com.caffidev.unoone;
+
+public class Board {
+    
+}
---
+++ Uno/core/src/com/caffidev/unoone/Game.java
@@ -0,0 +1,28 @@
+package com.caffidev.unoone;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Game extends ApplicationAdapter {
+	@Override
+	public void create(){ }
+	
+	@Override
+	public void resize(int width, int height){ 
+		
+	}
+	
+	@Override
+	public void render() { }
+	
+	@Override
+	public void pause() { }
+	
+	@Override
+	public void resume() { }
+	
+	@Override
+	public void dispose() { }
+}
